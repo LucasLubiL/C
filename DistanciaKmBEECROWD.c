c95ff42ece78920f5bf43f974b24a7a15c618a16
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-03-07 11:40:21 -0300

    Add files via upload

---
+++ DistanciaKmBEECROWD.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+
+    int d;
+    int aux;
+
+    scanf("%d",&d);
+
+    aux=d*2;
+
+    printf("%d minutos\n",aux);
+
+    return 0;
+}
+
+
