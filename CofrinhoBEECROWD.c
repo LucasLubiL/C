1f22c7cf12f408fd70fd5bc1af48217292f18a5d
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-03-10 23:11:18 -0300

    Add files via upload

---
+++ CofrinhoBEECROWD.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    int N,J,Z,cont=1;
+    int contaUM=0,totalJ=0,totalZ=0;
+
+    scanf("%d",&N);
+
+    while(N!=0){
+
+      printf("Teste %d\n",cont);
+
+      for(int i=0;i<N;i++){
+
+          scanf("%d %d",&J,&Z);
+
+          totalJ=totalJ+J;
+          totalZ=totalZ+Z;
+
+          contaUM=totalJ-totalZ;
+
+          printf("%d\n",contaUM);
+
+      }
+      cont++;
+      scanf("%d",&N);
+
+    }
+
+    return 0;
+}
+
+
+
+
+
+
