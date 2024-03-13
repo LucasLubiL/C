b742eaeafc10b962c5709dd3c21b47a930f83ea6
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-03-13 17:37:18 -0300

    Add files via upload

---
+++ ConversaoTempoBEECROWD.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(){
+
+    int N,h,m,s;
+
+    scanf("%d",&N);
+
+    h=N/3600;
+    m=(N%3600)/60;
+    s=(N%3600)%60;
+
+    printf("%d:%d:%d\n",h,m,s);
+
+    return 0;
+}
+
+
+
+
+
+
